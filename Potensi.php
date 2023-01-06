560b458c0db1feaa5a8154b271ed7a7b82d0256b
vianneyriska <122085860+vianneyriska@users.noreply.github.com>
2023-01-06 15:53:45 +0700

    Add files via upload

---
+++ Potensi.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+      <title>Kecamatan Sabbang Selatan</title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <link rel="stylesheet" href="css/bootstrap.min.css">
+      <link rel="stylesheet" href="css/style.css">
+      <link rel="stylesheet" href="css/responsive.css">
+      <link rel="icon" href="images/fevicon.png" type="image/gif" />
+      <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
+      <link rel="stylesheet" href="css/owl.carousel.min.css">
+      <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    </head>
+   <body class="main-layout">
+      <img src="headersabbangselatan.PNG" alt="#" style="width: 100%;">
+      <header>
+         <div class="header">
+            <div class="header_white_section">
+               <div class="container-fluid">
+                  <div class="row">
+                     <div class="col-md-12">
+                        <div class="header_information">
+                           <ul>
+                              <li><a href="Profil.php">Beranda</a></li>
+                              <li><a href="Potensi.php">Potensi</a></li>
+                              <li><a href="penduduk.php">Jumlah Penduduk</a></li>
+                              
+                           </ul>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+          
+         </div>
+         <!-- end header inner -->
+      </header>
+      <!-- end header -->
+      
+      
+      <div class="Tours">
+         <div class="container">
+           <br>
+           <br>
+           <br>
+               <div class="traveling-box">
+                      
+                     <div class="read-more">
+                      <a href="https://arcg.is/1eeiDL0">Lihat Potensi</a>&emsp;&emsp; 
+                         
+                     </div>
+
+                  </div>
+         </div>
+      </div>
+    
+<br>
+<br>
+<br>
+<br>
